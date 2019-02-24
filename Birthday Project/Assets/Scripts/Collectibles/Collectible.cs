d6f67b9af3854ac1d24f32c22bdb75fe2a7d94cc
Andrej Petelin <petelin.andrej@amis.net>
2019-02-24 05:02:25 +0100

    Added particles to collectibles

--- Birthday Project/Assets/Scripts/Collectibles/Collectible.cs
+++ Birthday Project/Assets/Scripts/Collectibles/Collectible.cs
@@ -5,20 +5,24 @@ using Events;
 
 public class Collectible : MonoBehaviour
 {
-    public ParticleSystem particles;
-    public float particlesDuration = 1f;
-
+    [Tooltip("GameEvent scriptable object to trigger the sound in SFXPlayer")]
     public GameEvent soundEvent;
 
+    [Tooltip("estimated 'padded' lifetime of particles, used so GameObject isn't destroyed while particles are alive")]
+    public float particlesLifetime = 2f;
+
+    // set to false once effects are being run to prevent multiple effects running from single collectible
     bool isAlive = true;
 
     // using this to hide the mesh while still leaving the collectible around for the particles to play
     new MeshRenderer renderer;
 
+    ParticleSystem particles;
     // Start is called before the first frame update
     void Start()
     {
         renderer = GetComponentInChildren<MeshRenderer>();
+        particles = GetComponentInChildren<ParticleSystem>();
     }
 
 
@@ -27,9 +31,9 @@ public class Collectible : MonoBehaviour
         if (isAlive)
         {
             renderer.enabled = false;
-            // if (particles) particles.Play();
+            if (particles) particles.Play();
             soundEvent.Raise();
-            Destroy(gameObject, particlesDuration);
+            Destroy(gameObject, particles.main.duration + particlesLifetime);
 
             // prevent multiple effects being run from one Collectible
             isAlive = false;
