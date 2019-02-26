ee8466393386a845d98bd89f77eba3907dd5eb0a
Irene Naya <irenayamackay@gmail.com>
2019-02-25 23:40:02 -0300

    Party Scene updated: cinemachine, postprocessing, lights

---
+++ Birthday Project/Assets/Scripts/SFX/DiscoBallRotator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscoBallRotator : MonoBehaviour
+{
+    public float rotationSpeed;
+    public GameObject ballLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(transform.up * rotationSpeed * Time.deltaTime);
+    }
+
+    public void StartRotating()
+    {
+        rotationSpeed = 45f;
+        ballLight.SetActive(true);
+    }
+}
---
+++ Birthday Project/Assets/Scripts/SFX/PostProcessingToggle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostProcessingToggle : MonoBehaviour
+{
+    public GameObject day;
+    public GameObject night;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleEffects()
+    {
+        day.SetActive(false);
+        night.SetActive(true);
+    }
+}
