de9d9367d9cf68b55d8988831d6ec45d526b728e
Irene Naya <irenayamackay@gmail.com>
2019-02-26 02:40:24 -0300

    not too pretty UIs added

---
+++ Birthday Project/Assets/Scripts/MazeCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCanvas : MonoBehaviour
+{
+    public float waitTime = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(KillObject());
+    }
+
+   IEnumerator KillObject()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+}
