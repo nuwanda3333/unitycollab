6e79d811a0c5044b5f6d833231b800b9895564ee
Aslı Tümerkan <aslitumerkan@gmail.com>
2019-02-26 23:24:44 +0300

    blow out candles

---
+++ Birthday Project/Assets/Scripts/BlowOutCandles.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowOutCandles : MonoBehaviour
+{
+    public AudioSource blowingCandles;
+    
+
+        public void OnMouseDown()
+        {
+             if (gameObject.activeInHierarchy == true)
+                 {
+                     gameObject.SetActive(false);
+                     blowingCandles.Play();
+                }
+             
+
+    }
+
+
+
+    
+
+
+
+
+}
