d409104186610fa565674f82a46dff7b5af28b9e
Irene Naya <irenayamackay@gmail.com>
2019-02-26 04:24:08 -0300

    sceneloader and statues added

---
+++ Birthday Project/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public string nextScene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(LoadNextSceneAsync());
+        }
+    }
+
+    IEnumerator LoadNextSceneAsync()
+    {
+        AsyncOperation loading = SceneManager.LoadSceneAsync(nextScene);
+        while (!loading.isDone)
+        {
+            Debug.Log("Loading");
+            yield return null;
+        }
+    }
+
+}
