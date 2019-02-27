27f30c7205fd27113b68700de2c8fddfdfe57b32
Irene Naya <irenayamackay@gmail.com>
2019-02-27 00:22:22 -0300

    lights added, clapping and blowing added

--- Birthday Project/Assets/Scripts/BlowOutCandles.cs
+++ Birthday Project/Assets/Scripts/BlowOutCandles.cs
@@ -1,28 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Events;
 
 public class BlowOutCandles : MonoBehaviour
 {
     public AudioSource blowingCandles;
-    
+    public GameEvent candleBlown;
 
-        public void OnMouseDown()
-        {
-             if (gameObject.activeInHierarchy == true)
-                 {
-                     gameObject.SetActive(false);
-                     blowingCandles.Play();
-                }
-             
+    static int blown;
+
+    private void Update()
+    {
 
     }
 
+    public void OnMouseDown()
+    {
+         if (gameObject.activeInHierarchy == true)
+         {
+            ++blown;
+            if (blown >= 4)
+            {
+                Debug.Log("RAISE");
+                candleBlown.Raise();
+            }
+            gameObject.SetActive(false);
+            blowingCandles.Play();
 
+            Debug.Log("Blown: " + blown);
 
-    
+        }
+             
 
-
-
-
+    }
 }
