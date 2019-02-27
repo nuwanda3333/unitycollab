9a7c8b3ed32e1bb6867298c358e12f418b0ff95f
Andrej Petelin <petelin.andrej@amis.net>
2019-02-27 16:23:34 +0100

    disabled running when in last corridor to allow sounds to play out at normal speed

---
+++ Birthday Project/Assets/Scripts/DisableRunning.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * A massive last-minute hack to disable running on the ThirdPersonUserControl
+ * 
+ * Random quote: "You're far to keen on where and how, but not so hot on why" - Ted Nealey - Jesus Christ Superstar
+ */
+
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class DisableRunning : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // We're really running out of time! Don't judge! :P
+            var user = other?.GetComponent<ThirdPersonUserControl>();
+            if (user) user.dontRun = true;
+        }
+    }
+}
