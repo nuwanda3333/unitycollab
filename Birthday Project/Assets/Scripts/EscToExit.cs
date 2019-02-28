02a2bf3099752c16520a77891780f8a9f8f96778
Andrej Petelin <petelin.andrej@amis.net>
2019-02-28 05:24:11 +0100

    Added GameManager with EscToExit script, used to detect the escape key and quit application when Esc has been hit

---
+++ Birthday Project/Assets/Scripts/EscToExit.cs
@@ -0,0 +1,18 @@
+﻿/**
+ * Last second hack to allow for exit with Esc key pressed!
+ * 
+ * Random quote: "Adriaaaaan!!!" - Rocky Balboa
+ */
+
+
+using UnityEngine;
+
+public class EscToExit : MonoBehaviour
+{
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) Application.Quit();
+    }
+}
